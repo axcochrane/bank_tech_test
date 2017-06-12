ca5f6d1ae30c690a956ccb8025c6f8d43dba2c54
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-06-12 13:20:16 +0100

    passing unit test that Bank Account can take starting balance as an argument

--- spec/BankAccount_spec.rb
+++ spec/BankAccount_spec.rb
@@ -3,5 +3,10 @@ describe 'BankAccount' do
     test_account = BankAccount.new
     expect(test_account.balance).to eq(0)
   end
+
+  it 'can take a starting balance as an argument' do
+    test_account = BankAccount.new(1000)
+    expect(test_account.balance).to eq(1000)
+  end
 end
 
