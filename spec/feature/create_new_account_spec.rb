92f0d267855e5a405bc775758d005c2bfdb5e545
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-06-12 12:34:01 +0100

    changed feature test to use 'to be_a' matcher

--- spec/feature/create_new_account_spec.rb
+++ spec/feature/create_new_account_spec.rb
@@ -1,6 +1,8 @@
+require_relative '../../model/BankAccount'
+
 feature 'Create new account' do
   scenario 'user can create a new bank account' do
     test_account = BankAccount.new 
-    expect(test_account).to eq true
+    expect(test_account).to be_a(BankAccount)
   end
 end
